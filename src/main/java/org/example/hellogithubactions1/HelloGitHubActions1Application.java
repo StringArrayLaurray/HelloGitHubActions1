e0eab9ad3f0fcacf76dd783ca0c38f91a7d52851
StringArrayLaurray <laan0004@stud.kea.dk>
2024-09-26 14:18:48 +0200

    Initial commit

---
+++ src/main/java/org/example/hellogithubactions1/HelloGitHubActions1Application.java
@@ -0,0 +1,13 @@
+package org.example.hellogithubactions1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitHubActions1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGitHubActions1Application.class, args);
+    }
+
+}
